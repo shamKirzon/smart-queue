c71ad804242132111687231b59d704198d1c48b7
shammy14-1 <shammysuyat@gmail.com>
2025-03-31 17:43:03 +0800

    chore: setup backend and frontend

---
+++ be/index.ts
@@ -0,0 +1,16 @@
+import cors from "cors"
+import express from "express"
+import dotenv from "dotenv"
+import { setupWebSocket } from "./websockets/setUpWebSocket"
+import http from 'http'
+
+dotenv.config(); 
+const app = express(); 
+const PORT = process.env.PORT; 
+const server = http.createServer(app)
+
+setupWebSocket(server)
+app.use(express.json()); 
+app.use(cors())
+
+console.log("solid ako, pogi na cute pa")
---
+++ fe/websocket/useWebSocket.ts
@@ -0,0 +1,33 @@
+import { View, Text } from "react-native";
+import { useRef, useEffect } from "react";
+
+const useWebSocket = (url: string) => {
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    if (ws.current) return;
+
+    ws.current = new WebSocket(url);
+    ws.current.onopen = () => console.log("Connected to websocket!");
+    ws.current.onmessage = (event) => {
+      console.log(`Received:  ${event.data}`);
+    };
+    ws.current.onerror = (error) => console.error("Websocket error: ", error);
+    ws.current.onclose = () => {
+      console.log("disconnected to websocket");
+      ws.current = null;
+    };
+
+    return () => {
+      ws.current?.close();
+    };
+  }, []); // ensuring our websocket initiliazes once
+
+  const sendMessage = (message: string) => {
+    ws.current?.send(message);
+  };
+
+    return{sendMessage}
+};
+
+export default useWebSocket;
