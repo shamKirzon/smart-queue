32074a28be9052d25cba6ea8e49b0197cbd55bbf
shammy14-1 <shammysuyat@gmail.com>
2025-04-08 23:03:45 +0800

    chore: setup

--- fe/screens/TransactionScreen.tsx
+++ fe/screens/TransactionScreen.tsx
@@ -1,7 +1,7 @@
 //di ko alam HAHAHAAHHAA
 import { View,Text,ScrollView,Dimensions,TouchableOpacity, Modal } from 'react-native'
 import React, { useState } from 'react';
-import { ReceiptProps } from '../types/receiptProps'
+import { receiptProps } from '../types/receiptProps';
 import {format} from "date-fns"
 import Back from "../assets/icons/back.svg";
 import Transactionbg from "../assets/backgrounds/transactionscreen-background.svg";
@@ -41,7 +41,7 @@ const textL = width * 0.08;
 
 interface TransactionProps{
   navigation: any, 
-  updateCustomerInfo: (CustomerInfo: ReceiptProps) => void; 
+  updateCustomerInfo: (CustomerInfo: receiptProps) => void;   
 }
 
 const ActionButton: React.FC<{ 
