1e37cc43e1b892badcd4a21a9d2ea759629ce62b
shammy14-1 <shammysuyat@gmail.com>
2025-05-24 13:53:35 +0800

    ...

--- fe/App.tsx
+++ fe/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
 
   // set initial route based on platform if web or mobile para di na mahirapan isetup sa web
   const initialRouteName =
-    Platform.OS === "web" ? "MonitorScreen" : "TellerHomeScreen";
+    Platform.OS === "web" ? "MonitorScreen" : "WelcomeScreen";
 
   return (
     <WebSocketProvider>
