ac20b6ff27c10dba4bb50b0ece9b183294c0da4b
shammy14-1 <shammysuyat@gmail.com>
2025-05-25 21:37:31 +0800

    pang anti merge lang to

--- be/database/connection.ts
+++ be/database/connection.ts
@@ -5,8 +5,8 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "smart_queue",
-  password: "rhenz",  
-  //password: "ymmahs13",
+  password: "ymmahs13",  
+  //password: "rhenz",
   port: 5432,
 });
 
